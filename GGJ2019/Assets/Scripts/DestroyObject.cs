ec711da5052cd326e6b131addf10721ada3811db
IablonschiEduard <32357086+IablonschiEduard@users.noreply.github.com>
2019-01-26 15:56:36 +0000

    Added collectibles*
    * = objects which can be picked up only when the player is inside the collider AND when the action key is pressed (in this case, E)

---
+++ GGJ2019/Assets/Scripts/DestroyObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour
+{
+    public bool collides;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (collides == true && Input.GetKeyDown(KeyCode.E))
+            Destroy(gameObject);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            collides = true;
+        }
+    }
+}
