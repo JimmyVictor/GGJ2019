61a5b038383ad865b6e0eb5b8c780d03e7a96f1b
Greyward <32356957+JimmyVictor@users.noreply.github.com>
2019-01-26 12:34:30 +0000

    Movement
    movement + basic scene

---
+++ GGJ2019/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField] // shows up in inspector even if private
+    private float speed = 1f;
+
+    [SerializeField]
+    private float lookSensitivity = 5f;
+
+
+    private PlayerMotor motor;
+
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        // Calculate movement velocity as a 3D vector
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 movHorizontal = transform.right * xMov;
+        Vector3 movVertical = transform.forward * zMov;
+
+        // Final movement vector
+        Vector3 _velocity = (movHorizontal + movVertical).normalized * speed;
+
+        // Apply movement
+        motor.Move(_velocity);
+
+        // Calculate rotation as a 3D vector (turning around)
+        float yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0f, yRot, 0f) * lookSensitivity;
+
+        //Apply rotation
+        motor.Rotate(_rotation);
+
+        // Calculate camera rotation as a 3D vector
+        float xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _cameraRotation = new Vector3(xRot, 0f, 0f) * lookSensitivity;
+
+        //Apply rotation
+        motor.RotateCamera(_cameraRotation);
+    }
+}
