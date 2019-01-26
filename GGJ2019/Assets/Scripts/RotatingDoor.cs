8cf0e0731f34987d9e127e42fe5b82e01ed3bcf9
IablonschiEduard <32357086+IablonschiEduard@users.noreply.github.com>
2019-01-26 16:56:34 +0000

    Added a door prototype
    The door can be opened (rotated at 90 degrees) by pressing E when the player is in its vicinity

---
+++ GGJ2019/Assets/Scripts/RotatingDoor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingDoor : MonoBehaviour
+{
+    public bool collides;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (collides == true && Input.GetKeyDown(KeyCode.E))
+        {
+            float yRotation = 90.0f;
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, yRotation, transform.eulerAngles.z);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            collides = true;
+        }
+    }
+}
+
