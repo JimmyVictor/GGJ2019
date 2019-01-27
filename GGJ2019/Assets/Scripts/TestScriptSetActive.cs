ead06691c2bc662dfdbedd00b486cfb8d2e24d37
IablonschiEduard <32357086+IablonschiEduard@users.noreply.github.com>
2019-01-27 12:58:00 +0000

    ramane de fixat dialogue-activation
    de vazut comentariul din scriptul cu "test"

---
+++ GGJ2019/Assets/Scripts/TestScriptSetActive.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScriptSetActive : MonoBehaviour {
+
+    public bool collides;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (collides == true && Input.GetKeyDown(KeyCode.E))
+            gameObject.SetActive(false);/*ramane de adaugat cod pt activat canvasul cu dialog cand playerul intra in collision box,
+                                          face o algere, si canvasul e dezactivat dupa + de dezactivat/distrus obiecte de pe mapa in functie de alegere*/
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            collides = true;
+        }
+    }
+}
