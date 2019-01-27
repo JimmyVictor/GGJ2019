43e3fd62f0d05c58d8f99a3c94f421e58d8a3fd6
IablonschiEduard <32357086+IablonschiEduard@users.noreply.github.com>
2019-01-27 14:58:28 +0000

    canvas deactivated
    canvas will be deactivated once player steps in a specific collision box and presses the E key. to fix : canvas should show up instead of be deactivated when player is inside collision box, canvas should be deactivated only when player clicks on a choice

--- GGJ2019/Assets/Scripts/ChoiceScript.cs
+++ GGJ2019/Assets/Scripts/ChoiceScript.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ChoiceScript : MonoBehaviour {
+public class ChoiceScript : MonoBehaviour
+{
 
+    public bool collides;
+    //public Canvas canvas;
+    
     public GameObject TextBox;
     public GameObject Choice01;
     public GameObject Choice02;
     public int ChoiceMade;
+
 
     public void ChoiceOption1()
     {
@@ -24,15 +29,31 @@ public class ChoiceScript : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(ChoiceMade >= 1)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ChoiceMade >= 1)
         {
             Choice01.SetActive(false);
             Choice02.SetActive(false);
         }
-	}
+        if (collides == true && Input.GetKeyDown(KeyCode.E))
+        {
+            /*canvas = GameObject.FindGameObjectWithTag("Canvas1").GetComponent<Canvas>();
+            canvas.SetActive(false);*/
+            GameObject.FindGameObjectWithTag("Canvas1").SetActive(false);/*de lasat canvasul transparent pe ecran pe parcursul intregului joc si de schimbat textul doar...?
+            ramane de adaugat cod pt activat canvasul cu dialog cand playerul intra in collision box, face o algere, si canvasul e dezactivat dupa + de dezactivat/distrus obiecte
+            de pe mapa in functie de alegere*/
+                                          
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            collides = true;
+        }
+    }
 }
